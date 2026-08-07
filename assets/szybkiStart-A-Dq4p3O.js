ce7f4bac7252a51c05e3fcce714549dcb1d19c84
Karl Evans <114938554+karlevans0110@users.noreply.github.com>
2026-08-07 20:17:40 +0200

    Poprawa

--- assets/szybkiStart-A-Dq4p3O.js
+++ assets/szybkiStart-A-Dq4p3O.js
@@ -1,6 +1,6 @@
 import{t as e}from"./navbar-B0BNu5GW.js";var t=class extends HTMLElement{connectedCallback(){let e=this.textContent||``;this.innerHTML=`
             <div class="kontener-pola-kodu">
                 <pre><code class="tresc-pola">${e}</code></pre>
-                <button class="guzik-kopiuj-pola-kodu"><img src="/public/ikonka-kopiuj.svg"></button>
+                <button class="guzik-kopiuj-pola-kodu"><img src="../ikonka-kopiuj.svg"></button>
             </div>
-        `,this.querySelector(`.guzik-kopiuj-pola-kodu`)?.addEventListener(`click`,()=>this.skopiujTresc())}async skopiujTresc(){let e=this.querySelector(`.tresc-pola`)?.textContent||``;await navigator.clipboard.writeText(e);let t=this.querySelector(`.guzik-kopiuj-pola-kodu img`);t&&(t.src=`/public/ikonka-ptaszek.svg`,t.classList.add(`efekt-skopiowano`),setTimeout(()=>{t.src=`../assets/ikonka-kopiuj.svg`,t.classList.remove(`efekt-skopiowano`)},1500))}};customElements.define(`pole-kodu`,t),e();
+        `,this.querySelector(`.guzik-kopiuj-pola-kodu`)?.addEventListener(`click`,()=>this.skopiujTresc())}async skopiujTresc(){let e=this.querySelector(`.tresc-pola`)?.textContent||``;await navigator.clipboard.writeText(e);let t=this.querySelector(`.guzik-kopiuj-pola-kodu img`);t&&(t.src=`../ikonka-ptaszek.svg`,t.classList.add(`efekt-skopiowano`),setTimeout(()=>{t.src=`../ikonka-kopiuj.svg`,t.classList.remove(`efekt-skopiowano`)},1500))}};customElements.define(`pole-kodu`,t),e();
