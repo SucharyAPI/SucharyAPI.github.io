27f697d7749afca31dc9a2040adefb56e8e589a0
Kacper Zagórny <kacper@MacBook-Air-Kacper.local>
2026-08-07 19:59:44 +0200

    Updates

---
+++ assets/index-DvnLjK83.js
@@ -0,0 +1,14 @@
+(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),t.credentials=e.crossOrigin===`use-credentials`?`include`:e.crossOrigin===`anonymous`?`omit`:`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();function e(){document.body.insertAdjacentHTML(`afterbegin`,`
+        <header>
+            <div class="nazwa-suchary">
+                <h1 class="nazwa-teksty tekst-suchary">SucharyAPI</h1>
+                <p class="nazwa-teksty">Ogólnopolska baza żartów</p>
+            </div>
+            <nav>
+                <a href="/" class="navbar-guziki">Główna</a>
+                <a href="/szybki-start/" class="navbar-guziki">Szybki start</a>
+                <a href="/zglos/" class="navbar-guziki">Zgłoś kawał</a>
+                <a href="/kontakt/" class="navbar-guziki">Kontakt</a>
+            </nav>
+        </header>
+    `)}var t=class extends Error{constructor(e){super(e),this.name=`SucharyError`}},n=class extends t{status;constructor(e,t){super(t),this.name=`HTTPError`,this.status=e}},r=class extends t{constructor(e=`Zapytanie przekroczyło limit czasu`){super(e),this.name=`TimeoutError`}},i=class extends t{constructor(e){super(e),this.name=`ConnectionError`}};async function a(e=null){let t=`https://sucharyapi.onrender.com`;e!==null&&(t=`https://sucharyapi.onrender.com/${e}`);let a=new AbortController,o=setTimeout(()=>a.abort(),6500);try{let e=await fetch(t,{signal:a.signal});if(clearTimeout(o),!e.ok)throw new n(e.status,`Serwer zwrócił kod błędu ${e.status}: ${e.statusText}`);return(await e.json()).tresc}catch(e){throw clearTimeout(o),e.name===`AbortError`?new r:e instanceof n?e:new i(e.message||`Błąd połączenia z serwerem`)}}e();var o=document.querySelector(`#guzik-losowania`),s=document.querySelector(`#pole-kawalu`);o?.addEventListener(`click`,l);async function c(){try{return await a()}catch(e){return e instanceof n?`Błąd serwera! Przepraszamy :(`:e instanceof r?`Wygląda na to że zbyt wiele osób chce się pośmiać. Spróbuj ponownie później :(`:e instanceof i?`Błąd połączenia! :( Sprawdź swój internet`:`Błąd! Przepraszamy :(`}}async function l(){if(!s)return;s.style.color=`black`,s.textContent=`Losowanie...`;let e=await c();e.includes(`:(`)?s.style.color=`red`:s.style.color=`black`,s.textContent=e}
